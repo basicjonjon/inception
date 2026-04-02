a04c806d92d15d903f193a68510efc924f8442b2
basic <basicjonjon@gmail.com>
2026-04-02 18:07:28 +0200

    error connexion bdd

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,13 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 # lancer mysql en background
 mysqld_safe &
 
-# attendre démarrage
-sleep 5
+# attendre mysql
+until mysqladmin ping --silent; do
+    sleep 1
+done
 
-mysql -e "CREATE DATABASE IF NOT EXISTS testDB;"
+# config DB
+mysql -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 
-wait
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 
-echo "salut"
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+
+# arrêter proprement
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+# lancer en foreground (important Docker)
+exec mysqld_safe
