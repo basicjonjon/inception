2cb261c4e3cfcfe7643887bc5aa74ee62ed6c2fc
basic <basicjonjon@gmail.com>
2026-04-03 17:37:20 +0200

    ca marche, voir secret et makefile

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# lancer mysql en background
 mysqld_safe &
 
-# attendre mysql
 until mysqladmin ping --silent; do
     sleep 1
 done
 
-# config DB
-mysql -u root <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" || true
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%'; FLUSH PRIVILEGES;"
 
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+mysqladmin -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-
-FLUSH PRIVILEGES;
-EOF
-
-# arrêter proprement
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
-
-# lancer en foreground (important Docker)
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -8,9 +8,9 @@ if [ ! -f wp-config.php ]; then
     tar -xvf latest.tar.gz --strip-components=1
     rm latest.tar.gz
     cp wp-config-sample.php wp-config.php
-    sed -i "s/database_name_here/${SQL_DATABASE}/" wp-config.php
-    sed -i "s/username_here/${SQL_USER}/" wp-config.php
-    sed -i "s/password_here/${SQL_PASSWORD}/" wp-config.php
+    sed -i "s/database_name_here/${MYSQL_DATABASE}/" wp-config.php
+    sed -i "s/username_here/${MYSQL_USER}/" wp-config.php
+    sed -i "s/password_here/${MYSQL_PASSWORD}/" wp-config.php
     sed -i "s/localhost/mariadb/" wp-config.php
 fi
 sed -i "s|listen = /run/php/php.*-fpm.sock|listen = 9000|" /etc/php/*/fpm/pool.d/www.conf
